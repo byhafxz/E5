2ddf743507779bdc4c5e3c2475ce786904f55d65
Hafis Suhaimi <hafizbinsuhaimi10@gmail.com>
2021-04-19 04:34:01 +0800

    Initial Commit

---
+++ E5/src/E5/Client/ClientLauncher.java
@@ -0,0 +1,21 @@
+package E5.Client;
+
+/**
+ * This class contains main() method to launch the client window
+ * @author emalianakasmuri
+ *
+ */
+public class ClientLauncher {
+
+	/**
+	 * Main entry point of client
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Launch client frame
+		ClientWordCounterFrame clientFrame = new ClientWordCounterFrame();
+		clientFrame.setVisible(true);
+	}
+
+}
